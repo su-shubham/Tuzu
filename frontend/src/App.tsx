ad8a0bbd37cbb8b10210ce974984600e905b9e96
Shubham <shubhamprajapati8268@gmail.com>
2023-08-02 15:57:57 +0530

    chore(add):frontend utils

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import ThemeProvider from "src/ThemeProvider"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+    </ThemeProvider>
   );
 }
 
